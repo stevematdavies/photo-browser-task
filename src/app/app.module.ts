9feb751e811aa0983d432debf2fc9b3a528902d3
stevematdavies <stevematdavies@gmail.com>
2018-12-06 17:16:59 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppMenuComponent } from './app-menu/app-menu.component';
 import { AppComponent } from './app.component';
-import { IconsModule } from './icons/icons.module';
+import { LogoComponent } from './logo/logo.component';
 import { PhotoBrowserComponent } from './photo-browser/photo-browser.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhotoBrowserComponent,
+    LogoComponent,
+    AppMenuComponent,
   ],
   imports: [
-    BrowserModule,
-    IconsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
